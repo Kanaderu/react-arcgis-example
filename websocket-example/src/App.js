ac5ab100b4c263ae21c5e7dbc1ce77219f412e3c
David Fan <fand.kanade@gmail.com>
2019-11-21 21:31:31 -0500

    Added initial websocket connection

--- websocket-example/src/App.js
+++ websocket-example/src/App.js
@@ -1,10 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { w3cwebsocket as W3CWebSocket } from "websocket";
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+const group_name = 'test'
+const client = new W3CWebSocket('ws://127.0.0.1:8088/ws/vehicles/' + group_name + '/');
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    }
+  }
+
+  UNSAFE_componentWillMount() {
+    client.onopen = () => {
+      console.log('Websocket Client Connected');
+    };
+    client.onmessage = (message) => {
+      console.log("Message!");
+      console.log(message);
+      const dataFromServer = JSON.parse(message.data);
+      console.log(dataFromServer);
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Essay:
+          </label>
+          <br />
+          <textarea value={this.state.value} onChange={this.handleChange} />
+          <br />
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
