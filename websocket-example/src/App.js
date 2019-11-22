0075be426c976e3ac446100831e1c9d4a3027eb4
David Fan <fand.kanade@gmail.com>
2019-11-21 22:57:09 -0500

    Added websocket interactions to example

--- websocket-example/src/App.js
+++ websocket-example/src/App.js
@@ -8,6 +8,12 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+        inputMsg: "",
+        inputLat: 0,
+        inputLon: 0,
+        msg: "",
+        lat: 0,
+        lon: 0,
     }
   }
 
@@ -15,24 +21,56 @@ class App extends Component {
     client.onopen = () => {
       console.log('Websocket Client Connected');
     };
+
+    client.onclose = () => {
+      console.log('Websocket closed unexpectedly');
+    };
+
     client.onmessage = (message) => {
       console.log("Message!");
       console.log(message);
       const dataFromServer = JSON.parse(message.data);
       console.log(dataFromServer);
+      this.setState({
+          msg: this.state.msg + dataFromServer.message + '\n',
+          lat: dataFromServer.lat,
+          lon: dataFromServer.lon,
+      });
     }
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('Submitting!');
+    client.send(JSON.stringify({
+      message: this.state.inputMsg,
+      lat: this.state.inputLat,
+      lon: this.state.inputLon,
+    }));
+  }
+
+  handleChange = (event) => {
+    console.log(event.target.id);
+    this.setState({
+        [event.target.name]: event.target.value
+    });
+    console.log(this.state);
   }
 
   render() {
     return (
       <div className="App">
-        <form onSubmit={this.handleSubmit}>
           <label>
             Essay:
-          </label>
+          </label><br />
+          <textarea value={this.state.msg} readOnly /><br />
+          <input type="number" readOnly value={this.state.lat} /><br />
+          <input type="number" readOnly value={this.state.lon} /><br />
           <br />
-          <textarea value={this.state.value} onChange={this.handleChange} />
-          <br />
+        <form onSubmit={this.handleSubmit}>
+          <input name="inputMsg" type="text" onChange={this.handleChange} /><br />
+          <input name="inputLat" type="number" onChange={this.handleChange} /><br />
+          <input name="inputLon" type="number" onChange={this.handleChange} /><br />
           <input type="submit" value="Submit" />
         </form>
       </div>
