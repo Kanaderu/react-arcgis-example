913d62fb1ddfb0251ed187890aab86cbfe0f63c8
David Fan <fand.kanade@gmail.com>
2019-11-21 19:53:20 -0500

    Cleanup

--- src/Point.js
+++ src/Point.js
@@ -7,39 +7,13 @@ const Point = (props) => {
     useEffect(() => {
 
         loadModules(['esri/Graphic']).then(([Graphic]) => {
-            /*
-            // Create a polygon geometry
-            const polygon = {
-                type: "polygon", // autocasts as new Polygon()
-                rings: [
-                    [-64.78, 32.3],
-                    [-66.07, 18.45],
-                    [-80.21, 25.78],
-                    [-64.78, 32.3]
-                ]
-            };
-            */
 
             // create a point
             const point = {
                 type: "point", // autocasts as new Point()
                 x: props.lon,
                 y: props.lat,
-                //x: -84.175444,
-                //y: 39.7346451
             };
-
-            /*
-            // Create a symbol for rendering the graphic
-            const fillSymbol = {
-                type: "simple-fill", // autocasts as new SimpleFillSymbol()
-                color: [227, 139, 79, 0.8],
-                outline: { // autocasts as new SimpleLineSymbol()
-                    color: [255, 255, 255],
-                    width: 1
-                }
-            };
-            */
 
             // Add the geometry and symbol to a new graphic
             const graphic = new Graphic({
@@ -57,6 +31,6 @@ const Point = (props) => {
 
     return null;
 
-}
+};
 
 export default Point;
