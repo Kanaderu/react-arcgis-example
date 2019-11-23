dcee5b4c378b1798a01029e57bb3fcaffbbe80e5
David Fan <fand.kanade@gmail.com>
2019-11-23 04:45:27 -0500

    Added working GPS example

---
+++ react-native/README.md
@@ -0,0 +1,7 @@
+# Console Debugging
+
+Android views `console.log` and other log messages can be found running:
+
+```
+adb logcat | grep ReactNativeJS
+```
--- react-native/gps/App.js
+++ react-native/gps/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import { Platform, StyleSheet, Text, View, PermissionsAndroid } from 'react-native';
+import Geolocation from 'react-native-geolocation-service';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -7,12 +8,65 @@ const instructions = Platform.select({
 });
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      location: {
+        timestamp: null,
+        coords: {
+          speed: null,
+          heading: null,
+          accuracy: null,
+          altitude: null,
+          longitude: null,
+          latitude: null,
+        }
+      }
+    }
+  }
+
+  componentDidMount() {
+    try {
+      /*
+      // Instead of navigator.geolocation, just use Geolocation.
+      const granted = PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION
+      )
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+      */
+      if (true) {
+        Geolocation.getCurrentPosition(
+          (position) => {
+            //console.log(position);
+            this.setState({
+              location: position
+            });
+          },
+          (error) => {
+            // See error code charts below.
+            console.log(error.code, error.message);
+          },
+          { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
+        );
+      }
+    } catch (err) {
+        console.warn(err);
+    }
+  }
   render() {
+    console.log(this.state);
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
+        <Text style={styles.instructions}>Timestamp: {this.state.location.timestamp}</Text>
+        <Text style={styles.instructions}>Speed: {this.state.location.coords.speed}</Text>
+        <Text style={styles.instructions}>Heading: {this.state.location.coords.heading}</Text>
+        <Text style={styles.instructions}>Accuracy: {this.state.location.coords.accuracy}</Text>
+        <Text style={styles.instructions}>Altitude: {this.state.location.coords.altitude}</Text>
+        <Text style={styles.instructions}>Longitude: {this.state.location.coords.longitude}</Text>
+        <Text style={styles.instructions}>Latitude: {this.state.location.coords.latitude}</Text>
       </View>
     );
   }
